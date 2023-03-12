fc2e50d70c3e8887021373f9412ee0bd62432a51
VladyslavBiriukov <vladislavbiriukovsquirrel@gmail.com>
2023-03-12 17:17:40 +0200

    -

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,29 +1,5 @@
+// Add imports above this line
 import { galleryItems } from './gallery-items';
-import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
+// Change code below this line
 
-
-const imageListEl = document.querySelector('.gallery');
-
-
-function createGalleryItemMarkup({ preview, original, description }) {
-  return `
-<a class="gallery__item" href="${original}">
-  <img class="gallery__image" src="${preview}" alt="${description}" />
-</a>
-  `;
-}
-
-function createGalleryMarkup(galleryItems) {
-    return galleryItems.map(createGalleryItemMarkup).join('');
-}
-
-
-imageListEl.insertAdjacentHTML('beforeend', createGalleryMarkup(galleryItems));
-
-    const lightbox = new SimpleLightbox('.gallery a', {
-  captionsData: "alt",
-  captionDelay: 250
-    });
-
-
+console.log(galleryItems);
